27c81ff006f0aaa9489cc791bec25ad0b15f677c
fortunajhoban-prog <fortunajhoban@gmail.com>
2026-05-02 04:57:18 +0800

    feat:Task 4-Resolved CORS
    Co-authored-by: Khiara Espelimbergo

---
+++ src/main/java/com/ws101/fortuna/EcommerceApi/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.ws101.fortuna.EcommerceApi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // allow all endpoints
+                        .allowedOrigins("http://localhost:5500")
+                        // frontend URL
+                        .allowedOrigins("http://127.0.0.1:5500")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
